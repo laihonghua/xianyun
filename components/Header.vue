243976199204f43ac54744cf9fa8189bf4dc3463
qianDuanAlai <654766591@qq.com>
2020-01-16 21:27:41 +0800

    轮播图完成

---
+++ components/Header.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="header-wrapper">
+    <el-row type="flex"
+            justify="space-between"
+            align="middle"
+            class="header">
+      <div class="logo">
+        <img src="../static/logo.jpg"
+             alt="">
+      </div>
+      <el-row type="flex"
+              class="navs">
+        <nuxt-link to="/">首页</nuxt-link>
+        <nuxt-link to="/post">旅游攻略</nuxt-link>
+        <nuxt-link to="/hotel">酒店</nuxt-link>
+        <nuxt-link to="/air">国内机票</nuxt-link>
+      </el-row>
+      <div class="user">
+        <nuxt-link to="user/login">登录/注册</nuxt-link>
+      </div>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="less">
+.header-wrapper {
+  height: 60px;
+  background: #fff;
+  border-bottom: 1px #eee solid;
+  box-shadow: 0 3px 2px #f6f6f6;
+  min-width: 1000px;
+  position: relative;
+  z-index: 2;
+}
+
+.header {
+  width: 1000px;
+  height: 60px;
+  margin: 0 auto;
+
+  .logo {
+    img {
+      width: 156px;
+      height: 42px;
+      display: block;
+    }
+  }
+
+  .navs {
+    margin-left: 20px;
+    flex: 1;
+
+    a {
+      display: block;
+      height: 60px;
+      line-height: 60px;
+      box-sizing: border-box;
+      padding: 0 20px;
+
+      &:hover {
+        border-bottom: 5px #409eff solid;
+        color: #409eff;
+      }
+    }
+
+    .nuxt-link-exact-active {
+      background: #409eff;
+      color: #fff;
+
+      &:hover {
+        color: #fff;
+      }
+    }
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,55 @@
 <template>
   <div>
+    <!-- 头部组件 -->
+    <Header />
+
+    <!-- 内容站位组件 -->
     <nuxt />
+    <foot />
   </div>
 </template>
+<script>
+import Header from '@/components/Header.vue'
+import foot from '@/components/foot.vue'
+export default {
+  components: {
+    Header,
+    foot
+  }
 
+}
+</script>
 
-<style>
+<style lang="less">
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
+ul,
+li,
+ol {
+  list-style: none;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
+a:hover {
+}
+em,
+i {
+  font-style: normal;
+}
 </style>
